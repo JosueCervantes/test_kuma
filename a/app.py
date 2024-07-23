f02bc89ec108240f04b23869308f3804220278a7
JosueCervantes <josuecervantesesparza123@gmail.com>
2024-07-23 09:19:37 -0600

    23julio

--- a/app.py
+++ a/app.py
@@ -1,24 +1,37 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template
 import requests
 
 app = Flask(__name__)
 
 # Configura la URL y los encabezados de la API de Uptime Kuma
-API_URL = 'http://localhost:3001/api/monitor'
+API_URL = 'http://localhost:3001/settings/api-keys'
 HEADERS = {
     'Content-Type': 'application/json',
-    'Authorization': 'Bearer <uk2_4DBZ4eqaqC1Ap0ksVF1HVZW24vaxN1M7Y8LrGvuT>'
+    'Authorization': 'Bearer <uk3__QPZ9esmnjsbWm5D4zuPgnhHtwnxUK1JFz4Ashj9>'
 }
 
 @app.route('/')
 def index():
-    response = requests.get(API_URL, headers=HEADERS)
-    monitors = response.json()
+    try: 
+        response = requests.get(API_URL, headers=HEADERS)
+        response.raise_for_status() 
+        print('Response status code:', response.status_code)
+        print('Response headers:', response.headers)
+        print('Response content:', response.text)
+        monitors = response.json()
+    except requests.exceptions.HTTPError as http_err:
+        print(f'HTTP error occured: {http_err}')
+        monitors = []
+    except requests.exceptions.RequestException as err:
+        print(f'Other error occured: {err}')
+        monitors = []
+    except ValueError as json_err:
+        print(f'JSON decode error: {json_err}')
+        print(f'Response content: ', response.text)
+        monitors = []
+
     return render_template('index.html', monitors=monitors)
 
 if __name__ == '__main__':
     app.run(debug=True)
 
-
-#Instalar python, pip y requests en Linux
-#Clonar el repositorio y ejecutar
